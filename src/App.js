3bd73860edf20e728d5394e78996e4188f0b9701
smritiriya <smritiriya7@gmail.com>
2023-07-06 12:35:33 +0530

    minor changes in team section

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Banner from './components/Banner';
+import Feed from './components/Feedback';
+import Footer from './components/Footer';
+import FAQ from './components/Faq';
+import WhatweOffer from './components/WhatWeOffer';
+import Contact from './components/Contact/Contact';
+import About from './components/About/About';
+import Team from './components/Team/Team';
+import { BrowserRouter } from 'react-router-dom';
+import { HashLink } from 'react-router-hash-link';
+import TeamSection from './components/Team';
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Banner />
+        <About />
+        <WhatweOffer />
+
+        {/* <Feed /> */}
+        <FAQ />
+        {/* <Team /> */}
+        <TeamSection/>
+        <Contact />
+
+        <Footer />
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/components/WhatWeOffer.js
@@ -0,0 +1,24 @@
+import React from "react";
+import OfferCard from "./Card/Offercard";
+import { WhatWeOffer } from "./config";
+// import color from "../../constant";
+// import OfferCard from "../../components/Card/OfferCard";
+// import { WhatWeOffer } from "../../config";
+
+function WhatweOffer() {
+  return (
+    <>
+      <div
+        className="container-fluid wallpaper row d-flex justify-content-between my-5 py-3 py-md-5 px-5 mx-0 offerbg"
+        id="myfeature"
+      >
+        <h1 className="Heading-1 text-center col-12 py-5" style={{color:"white"}}>What We Offer ?</h1>
+        {WhatWeOffer.map((items) => {
+          return <OfferCard data={items} />;
+        })}
+      </div>
+    </>
+  );
+}
+
+export default WhatweOffer;
